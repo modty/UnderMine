679318ec0ab3d657cf9d4dca1190324e34f020f1
1997Gongquan <2673077461@qq.com>
2020-09-28 21:28:28 +0800

    2020-09-28 远程攻击旋转

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 800 * Time.deltaTime);
+    }
+}
