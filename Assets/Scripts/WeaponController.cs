339ba8fbeffb0c6311b33af9f1688a9c9413554c
1997Gongquan <2673077461@qq.com>
2020-10-13 16:26:33 +0800

    2020-10-13 移动更改

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -8,41 +8,22 @@ public class WeaponController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public Vector2 torwadsPosition;
+    public float currentSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        preTime = Time.time;
+        InvokeRepeating(nameof(doWeapon),0,.01f);
     }
 
     private bool start=true;
     private bool isBack=false;
-    // Update is called once per frame
-    void Update()
-    {
-        if (start)
-        {
-            torwadsPosition=getTowardsPosition(rb.position);
-            start = false;
-        }
-        transform.Rotate(0, 0, 800 * Time.deltaTime);
-        if (isBack)
-        {
-            rb.position = Vector2.MoveTowards(rb.position,PlayerState.Instance.PlayerPosition, 0.2f);
-            if (rb.position == PlayerState.Instance.PlayerPosition)
-            {
-                isBack = false;
-                start = true;
-                gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            rb.position=Vector2.MoveTowards(rb.position,torwadsPosition,0.2f);
-            if (rb.position == torwadsPosition)
-            {
-                isBack = true;
-            }
-        }
-    }
+    private bool isAwake;
+    private float preTime;
+    
+    private float newTime;
+
+    private float frameTimes;
 
     private Vector2 MousePosition()
     {
@@ -55,6 +36,55 @@ public class WeaponController : MonoBehaviour
     {
         Vector2 vector2 = MousePosition();
         double c = Math.Atan2(vector2.y, vector2.x);
-        return new Vector2((float) (5*Math.Cos(c)),(float) (5*Math.Sin(c)));
+        return new Vector2((float) (Constants.WeaponThrowDistance*Math.Cos(c)),(float) (Constants.WeaponThrowDistance*Math.Sin(c)));
+    }
+
+    public float calculateDistance()
+    {
+        newTime = Time.time;
+        float times=newTime-preTime;
+        float v1 = -1;
+        if (isBack)
+        {
+            v1=currentSpeed + Constants.WeaponThrowAcceleration * times;
+        }
+        else
+        {
+            v1=currentSpeed - Constants.WeaponThrowAcceleration * times;
+        }
+        float distance = (currentSpeed + v1) * times / 2;
+        currentSpeed = v1;
+        preTime = newTime;
+        return distance;
+    }
+
+    public void doWeapon()
+    {
+        if (start)
+        {
+            torwadsPosition=getTowardsPosition(rb.position);
+            currentSpeed = Constants.WeaponThrowMaxSpeed;
+            preTime = Time.time;
+            start = false;
+        }
+        transform.Rotate(0, 0, Constants.WeaponThrowRotateSpeed * Time.deltaTime);
+        if (isBack)
+        {
+            rb.position = Vector2.MoveTowards(rb.position,PlayerState.Instance.PlayerPosition, .05f);
+            if (rb.position == PlayerState.Instance.PlayerPosition)
+            {
+                isBack = false;
+                start = true;
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            rb.position=Vector2.MoveTowards(rb.position,torwadsPosition,.05f);
+            if (rb.position == torwadsPosition)
+            {
+                isBack = true;
+            }
+        }
     }
 }
